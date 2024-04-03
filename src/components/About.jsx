c736e93728b3dca80fb2f51ff38b8d8e11e5a246
Shwet <shwetpatel2712@gmail.com>
2024-04-03 19:27:45 +0530

    modified image in About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,8 +4,8 @@ import Sectiontitle from "./Sectiontitle";
 const About = () => {
     return (
         <div className="bg-white py-20" id="about">
-            <div className="align-elements grid md: grid-cols-2 items-center gap-16">
-                <img src={aboutimg} alt="This is aout me."  className="h-64 w-full"/>
+            <div className="align-elements grid grid-cols-1 md:grid-cols-2 items-center gap-16">
+                <img src={aboutimg} alt="This is aout me."  className="hidden md:block md:h-64 md:w-full"/>
                 <div>
                     <Sectiontitle text="code & coffee" />
                     <p className=" text-slate-800 mt-8 leading-loose">
@@ -18,7 +18,7 @@ const About = () => {
                         about latest technology in domain of wed development.
                     </p>
                     <p className=" text-slate-800 mt-3 leading-loose">
-                    As of now i don't have any profesional working experience. Perhaps you can help me out.
+                    As of now i don't have any profesional working experience. May be you can help me out.
                     </p>
                 </div>
             </div>
